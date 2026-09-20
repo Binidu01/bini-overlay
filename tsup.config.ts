de31fc857c1ee6143d674376733560d701d8294d
Binidu01 <rbinidu@gmail.com>
2026-09-21 03:28:56 +0530

    release: v2.1.0

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,18 +3,13 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry      : ['src/index.ts'],
   format     : ['esm', 'cjs'],
-  dts        : { 
-    resolve: true,
-    compilerOptions: {
-      ignoreDeprecations: "6.0"
-    }
-  },
+  dts        : true,
   clean      : true,
   sourcemap  : true,
   splitting  : false,
   treeshake  : true,
   target     : 'es2022',
-  external   : ['vite'],
+  external   : ['vite', 'bini-router', '@jridgewell/trace-mapping'],
   esbuildOptions(opts) {
     opts.platform = 'node';
   },
